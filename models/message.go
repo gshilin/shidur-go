04265dcb36b61aa3e78f6f8baeb491d6290048a9
Gregory Shilin <gshilin@gmail.com>
2016-02-09 16:48:33 +0200

    add language to message

--- models/message.go
+++ models/message.go
@@ -5,13 +5,14 @@ import "time"
 // This is our single struct/model that is used with Gorm for all
 // DB functions.
 type Message struct {
-  ID        uint      `gorm:"primary_key" json:"id"`
-  CreatedAt time.Time                    `json:"-"`
-  UpdatedAt time.Time                    `json:"-"`
+	ID        uint      `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time                    `json:"-"`
+	UpdatedAt time.Time                    `json:"-"`
 
-  Message   string                   `json:"message"`
-  UserName  string    `sql:"size:255" json:"user_name"`
-  Type      string    `sql:"size:255" json:"type"`
+	Message   string                   `json:"message"`
+	UserName  string    `sql:"size:255" json:"user_name"`
+	Type      string    `sql:"size:255" json:"type"`
+	Language  string    `sql:"size:255" json:"language"`
 }
 
 type Messages []Message
